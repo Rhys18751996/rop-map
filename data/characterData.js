833da42302cee1346dc497e7497f54489d6914fc
Rhys18751996 <18751996@student.westernsydney.edu.au>
2025-12-19 08:54:34 +1100

    adding portrait field to DATA_CHARACTERS

--- data/characterData.js
+++ data/characterData.js
@@ -3,51 +3,58 @@ const DATA_CHARACTERS = {
         {
             "name": "Nori",
             "Series": "Rings of Power",
-            "color": "#2f4f22"
+            "color": "#2f4f22",
+            "portrait": "img/portraits/harfoots.webp"
         },
         {
             "name": "Galadriel",
             "Series": "Rings of Power",
             "seasons": [1, 2],
             "color": "#3c76b1",
-            "portrait": "galadriel.webp"
+            "portrait": "img/portraits/galadriel.webp"
         },
         {
             "name": "Elrond",
             "Series": "Rings of Power",
-            "color": "#907826"
+            "color": "#907826",
+            "portrait": "img/portraits/"
         },
         {
             "name": "Arondir",
             "Series": "Rings of Power",
-            "color": "#81639f"
+            "color": "#81639f",
+            "portrait": "img/portraits/"
         },
         {
             "name": "Elendil",
             "Series": "Rings of Power",
-            "color": "#494949"
+            "color": "#494949",
+            "portrait": "img/portraits/"
         },
         {
             "name": "Halbrand",
             "Series": "Rings of Power",
-            "color": "#a9531f"
+            "color": "#a9531f",
+            "portrait": "img/portraits/"
         },
         {
             "name": "Frodo and Sam",
             "Title": "The Lord of the Rings: The Fellowship of the Ring",
             "seasons": [100],
             "color": "#912222",
-            "portrait": "frodosam.webp"
+            "portrait": "img/portraits/frodosam.webp"
         },
         {
             "name": "Elrond",
             "Title": "The Lord of the Rings: The Fellowship of the Ring",
-            "color": "#907826"
+            "color": "#907826",
+            "portrait": "img/portraits/"
         },
         {
             "name": "Bilbo and Thorin",
             "Title": "The Hobbit",
-            "color": "#4e2291"
+            "color": "#4e2291",
+            "portrait": "img/portraits/"
         }
     ]
 };
