ca914bb00d7bf5a98cdb971ab7ad8ceeb8fc28e3
Rhys18751996 <18751996@student.westernsydney.edu.au>
2025-12-19 08:56:03 +1100

    adding portrait field to DATA_CHARACTERS

--- data/characterData.js
+++ data/characterData.js
@@ -3,37 +3,42 @@ const DATA_CHARACTERS = {
         {
             "name": "Nori",
             "Series": "Rings of Power",
+            "seasons": [1, 2, 3],
             "color": "#2f4f22",
             "portrait": "img/portraits/harfoots.webp"
         },
         {
             "name": "Galadriel",
             "Series": "Rings of Power",
-            "seasons": [1, 2],
+            "seasons": [1, 2, 3],
             "color": "#3c76b1",
             "portrait": "img/portraits/galadriel.webp"
         },
         {
             "name": "Elrond",
             "Series": "Rings of Power",
+            "seasons": [1, 2, 3],
             "color": "#907826",
             "portrait": "img/portraits/"
         },
         {
             "name": "Arondir",
             "Series": "Rings of Power",
+            "seasons": [1, 2, 3],
             "color": "#81639f",
             "portrait": "img/portraits/"
         },
         {
             "name": "Elendil",
             "Series": "Rings of Power",
+            "seasons": [1, 2, 3],
             "color": "#494949",
             "portrait": "img/portraits/"
         },
         {
             "name": "Halbrand",
             "Series": "Rings of Power",
+            "seasons": [1, 2, 3],
             "color": "#a9531f",
             "portrait": "img/portraits/"
         },
@@ -47,12 +52,14 @@ const DATA_CHARACTERS = {
         {
             "name": "Elrond",
             "Title": "The Lord of the Rings: The Fellowship of the Ring",
+            seasons: [100],
             "color": "#907826",
             "portrait": "img/portraits/"
         },
         {
             "name": "Bilbo and Thorin",
             "Title": "The Hobbit",
+            seasons: [104],
             "color": "#4e2291",
             "portrait": "img/portraits/"
         }
