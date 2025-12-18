7a44f34546ab623470adcd35fc6de67592f48ef1
Rhys18751996 <18751996@student.westernsydney.edu.au>
2025-12-19 08:48:30 +1100

    bringing back relevant data that I thought was unrelevant

--- data/seasonsData.js
+++ data/seasonsData.js
@@ -4,30 +4,35 @@ const DATA_SEASONS = [
         id: 1,
         name: "Rings of Power - Season 1",
         episodes: 8,
-        characters: ["Arondir","Elendil","Elrond","Galadriel","Halbrand","Nori"]
+        characters: ["Arondir","Elendil","Elrond","Galadriel","Halbrand","Nori"],
+        markersRelevant: marker => marker.episodes.some(e => e.season === 1)
     },
     {
         id: 2,
         name: "Rings of Power - Season 2",
         episodes: 8,
-        characters: ["Galadriel","Arondir"]
+        characters: ["Galadriel","Arondir"],
+        markersRelevant: marker => marker.episodes.some(e => e.season === 2)
     },
     {
         id: 3,
         name: "Rings of Power - Season 3",
         episodes: 8,
-        characters: ["Elendil","Elrond","Halbrand","Nori"]
+        characters: ["Elendil","Elrond","Halbrand","Nori"],
+        markersRelevant: marker => marker.episodes.some(e => e.season === 3)
     },
     {
         id: 100,
         name: "The Lord of the Rings (Movies)",
         episodes: 1,
-        characters: ["Frodo and Sam"]
+        characters: ["Frodo and Sam"],
+        markersRelevant: marker => marker.episodes.some(e => e.season === 100 || e.season === 101)
     },
         {
         id: 104,
         name: "The Hobbit (Movies)",
         episodes: 1,
-        characters: ["Bilbo and Thorin"]
+        characters: ["Bilbo and Thorin"],
+        markersRelevant: marker => marker.episodes.some(e => e.season === 104 || e.season === 106)
     }
 ];
---
+++ js/core/pathsUIController.js
@@ -0,0 +1,8 @@
+// rop-map/js/core/pathsUIController.js
+/*
+Responsibility:
+- DOM Logic, this will decide what character icons will be put onto html
+- Build checkboxes + labels
+- Filter characters by season
+- Reset paths when season changes
+*/
