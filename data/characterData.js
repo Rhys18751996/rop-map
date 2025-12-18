a14c55df48edd127580aa89ca6b344522b59d59a
Rhys18751996 <18751996@student.westernsydney.edu.au>
2025-12-18 15:47:30 +1100

    prototyping a character checkbox got pathsgrid

---
+++ data/characterData.js
@@ -0,0 +1,38 @@
+DATA_CHARACTERS = [
+    {
+        "name": "Nori",
+        "color": "#2f4f22"
+    },
+    {
+        "name": "Galadriel",
+        seasons: [1, 2],
+        "color": "#3c76b1",
+        portrait: "galadriel.webp"
+    },
+    {
+        "name": "Elrond",
+        "color": "#907826"
+    },
+    {
+        "name": "Arondir",
+        "color": "#81639f"
+    },
+    {
+        "name": "Elendil",
+        "color": "#494949"
+    },
+    {
+        "name": "Halbrand",
+        "color": "#a9531f"
+    },
+    {
+        "name": "Frodo and Sam",
+        seasons: [100],
+        "color": "#912222",
+        portrait: "frodosam.webp"
+    },
+    {
+        "name": "Bilbo and Thorin",
+        "color": "#4e2291"
+    }
+]
